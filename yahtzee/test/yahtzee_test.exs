8514785b379262f01b6fde9acea570d774c27e20
muhammadahmedut <muhammadahmed@ut.ee>
2025-09-30 15:41:50 +0300

    Three of a kind - failing

--- yahtzee/test/yahtzee_test.exs
+++ yahtzee/test/yahtzee_test.exs
@@ -5,4 +5,26 @@ defmodule YahtzeeTest do
   test "greets the world" do
     assert Yahtzee.hello() == :world
   end
+  
+  describe "score_lower/1 - Three of a kind" do
+    test "Identify 'Three of a kind' with ones" do
+      assert %{"Three of a kind": 17} = Yahtzee.score_lower([2, 3, 4, 4, 4])
+    end
+
+    test "Identify 'Three of a kind' with threes" do
+      assert %{"Three of a kind": 13} = Yahtzee.score_lower([3, 3, 3, 2, 2])
+    end
+
+    test "Identify 'Three of a kind' with fives" do
+      assert %{"Three of a kind": 25} = Yahtzee.score_lower([5, 5, 5, 5, 5])
+    end
+
+    test "No 'Three of a kind' present" do
+      assert %{"Three of a kind": 0} = Yahtzee.score_lower([1, 2, 3, 4, 5])
+    end
+
+    test "Exactly three of a kind, not four or five" do
+      assert %{"Three of a kind": 14} = Yahtzee.score_lower([2, 2, 2, 4, 4])
+    end
+  end
 end
